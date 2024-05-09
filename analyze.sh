e11c731be494435f19e7473119def0779ee7505d
Jean-François Lépine <jeanfrancois.lepine@bluspark.io>
2024-05-09 19:04:55 +0200

    analyze with lizard

--- analyze.sh
+++ analyze.sh
@@ -1,7 +1,47 @@
 set -e
 
+# if env == "dev" then
+url="@todo"
+if [ "$ENV" == "dev" ]; then
+    echo "Running in dev mode"
+    url="http://localhost:8000/webhook/github/action/lizard?"
+fi
+
+# add current git commit hash to the url
+url="$url&ref=$(git rev-parse HEAD)"
+
+# add current branch
+url="$url&branch=$(git rev-parse --abbrev-ref HEAD)"
+
+# add current repo
+url="$url&repo=$(git config --get remote.origin.url)"
+
+echo "URL: $url"
+
+
 # Install lizard
-pip install lizard
+installed=$(pip show lizard)
+if [ -z "$installed" ]; then
+    echo "Installing lizard"
+    pip install lizard
+fi
 
 # Start the analysis
-lizard --output_file=lizard.csv
+lizard \
+    --output_file=lizard.csv \
+    --exclude "**/node_modules/*" \
+    --exclude "**/vendor/*" \
+    --exclude "**/var/*" \
+    --exclude "**/cache/*" \
+    --csv \
+
+# Send the results to the server
+response=$(curl \
+    -X POST $url \
+    --fail \
+    --insecure \
+    -L \
+    -H "Content-Type: text/csv" \
+    --data-binary @lizard.csv)
+
+echo "Response: $response"
